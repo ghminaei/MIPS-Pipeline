9b2f2d0a8385629aa66bedad6189a144acee5fd2
ghminaei <ghminaei1037@gmail.com>
2019-05-22 21:35:31 +0430

    initial files added

---
+++ mux3.v
@@ -0,0 +1,20 @@
+`timescale 1ps/1ps
+module mux3 (
+    sel1,
+    sel2,
+    inp1,
+    inp2,
+    inp3,
+    out
+    );
+    parameter n;
+    input sel1,
+    sel2,
+    sel3;
+    input [n-1:0]inp1, inp2, inp3;
+    output [n-1:0]out;
+    assign out = {sel1, sel2} == 2'b00 ? inp1 : 
+                 {sel1, sel2} == 2'b01 ? inp2 : 
+                 {sel1, sel2} == 2'b10 ? inp3 :
+                 out;
+endmodule
---
+++ testBench.v
@@ -0,0 +1,19 @@
+`timescale 1ps/1ps
+module TestBench ();
+    parameter CLK = 100;
+    reg clk = 0;
+    reg rst = 0;
+    piplineMips UUT(
+        .clk(clk),
+        .rst(rst)
+    );
+    always #CLK clk = ~clk;
+    initial begin 
+        #(CLK) rst = 1;
+        #(3*CLK) rst = 0;
+        #(313*CLK)
+        $stop;
+    end
+
+
+endmodule
