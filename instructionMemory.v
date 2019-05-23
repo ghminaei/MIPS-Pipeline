02714ab8eed34d9f4d747d30eea92444b17dcee2
Zahra Moosavi <moosavizahra67@yahoo.com>
2019-05-23 20:18:31 +0430

    instruction memory is updated

--- instructionMemory.v
+++ instructionMemory.v
@@ -5,13 +5,13 @@ module InstMemory (
     );
     parameter WORD = 8, LENGTH = 32'd4294967295, PCL = 32;
     input [PCL-1:0]address;
-    output [WORD-1:0]out;
+    output [4*WORD-1:0]out;
     reg [WORD-1:0]memory[LENGTH-1:0];
-    parameter NOP = 16'b1000000001000000;
+    parameter NOP = 32'b00000100000000000000000000000000;
     integer i;
     initial begin
-        for (i = 0; i < LENGTH; i = i + 1) begin
-            memory[i] = NOP;
+        for (i = 0; i < LENGTH; i = i + 4) begin
+            {memory[i], memory[i+1], memory[i+2], memory[i+3]} = NOP;
         end
     end
 
