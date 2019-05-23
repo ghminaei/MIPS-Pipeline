d937d1819e5d1c625e2988881b5a71f416db72f8
ghminaei <ghminaei1037@gmail.com>
2019-05-23 16:26:13 +0430

    Sign Extender added

---
+++ SignExtender.v
@@ -0,0 +1,16 @@
+`timescale 1ps/1ps
+module SignExtender (
+    inp,
+    out
+    );
+    parameter n = 32, SIGNBit = 16;
+    input [n-1:0]inp;
+    output [n-1:0]out;
+    wire ext;
+    assign ext = inp[n-1] ? {(SIGNBit){1'b1}} : 
+                 {(SIGNBit){1'b0}};
+    
+    assign out = {ext, inp};
+    
+endmodule
+                  
